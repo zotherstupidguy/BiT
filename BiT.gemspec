a74e05887434bebb1bfc2332fa6dca30bf90b138
zotherstupidguy <zotherstupidguy@gmail.com>
2016-12-19 11:06:59 +0200

    added basic code

--- BiT.gemspec
+++ BiT.gemspec
@@ -34,4 +34,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "minitest", "~> 5.0"
   spec.add_dependency 'mushin'
+  spec.add_dependency 'mushin_ext_ssd'
+  spec.add_dependency 'mushin_ext_tpb'
 end
--- lib/mushin_dsf_bit.rb
+++ lib/mushin_dsf_bit.rb
@@ -1,22 +1,37 @@
 require 'mushin'
-require_relative 'BiT/version'
+require 'mushin_ext_tpb'
+require 'mushin_ext_ssd'
 
+require_relative 'BiT/version'
 
 module BiT
   # Usage:
-# BiT::Domain.new do
-# # use your BiT DSL here 
-# end
-class Domain
-# Define your BiT DSL here
-      using Mushin::Domain
-      context :change_me do
-	construct :change_me do
-	  use :change_me, opts: {}, params: {}
-	  use :change_me, opts: {}, params: {}
-	  use :change_me, opts: {}, params: {}
-	end
-      end
-end
+  # BiT::Domain.new do
+  #  all do
+  #    search          query: "opencourseware MIT" 
+  #    download        magnet: "somebighash" 
+  #  end 
+  #
+  #  server do
+  #	seed            path: "somepathtofolder" 
+  #  end 
+  #
+  #  query do
+  #     seeded
+  #	recently_deleted    
+  #  end 
+  # end
 
+  class Domain
+    # Define your BiT DSL here
+    using Mushin::Domain
+
+    context :all do # all websites
+      construct :search do
+	use ext: TPB::Ext                   , opts: {:max_results => :max_results}                  , params: {:query => :query}
+	use ext: SSD::Ext                   , opts: {:path => :ssd_path, :cqrs => :cqrs_command}    , params: {:id => :ssd_query}
+      end
+    end
+
+  end
 end
